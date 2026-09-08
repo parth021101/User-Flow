207ff1defbacfaea9002f9066168d05d82f8c984
Parth Parasiya <122023452+parth021101@users.noreply.github.com>
2026-09-09 02:17:09 +0530

    Update logo text to 'UserFlow' in NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = ({ setShowForm }) => {
   return (
     <div className="border-b border-[#807e7e] p-5 flex items-center justify-between">
       <h1 className="text-3xl font-medium tracking-wide whitespace-nowrap">
-        Logo
+        UserFlow
       </h1>
       <button onClick={handleClick} className="px-3 py-2 rounded-lg font-medium tracking-wide bg-blue-600 cursor-pointer transition hover:bg-blue-700">
         Create User
